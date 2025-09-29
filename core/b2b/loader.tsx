fc0da0929a109978741e75f619286c08bfa07658
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-09-29 14:49:17 -0500

    Support for custom production Buyer Portal build in loader scripts

--- core/b2b/loader.tsx
+++ core/b2b/loader.tsx
@@ -4,11 +4,13 @@ import { auth } from '~/auth';
 
 import { ScriptDev } from './script-dev';
 import { ScriptProduction } from './script-production';
+import { ScriptProductionCustom } from './script-production-custom';
 
 const EnvironmentSchema = z.object({
   BIGCOMMERCE_STORE_HASH: z.string({ message: 'BIGCOMMERCE_STORE_HASH is required' }),
   BIGCOMMERCE_CHANNEL_ID: z.string({ message: 'BIGCOMMERCE_CHANNEL_ID is required' }),
   LOCAL_BUYER_PORTAL_HOST: z.string().url().optional(),
+  PROD_BUYER_PORTAL_URL: z.string().url().optional(),
   STAGING_B2B_CDN_ORIGIN: z.string().optional(),
 });
 
@@ -17,10 +19,23 @@ export async function B2BLoader() {
     BIGCOMMERCE_STORE_HASH,
     BIGCOMMERCE_CHANNEL_ID,
     LOCAL_BUYER_PORTAL_HOST,
+    PROD_BUYER_PORTAL_URL,
     STAGING_B2B_CDN_ORIGIN,
   } = EnvironmentSchema.parse(process.env);
 
   const session = await auth();
+
+  if (PROD_BUYER_PORTAL_URL) {
+    return (
+      <ScriptProductionCustom
+        cartId={session?.user?.cartId}
+        channelId={BIGCOMMERCE_CHANNEL_ID}
+        storeHash={BIGCOMMERCE_STORE_HASH}
+        token={session?.b2bToken}
+        prodUrl={PROD_BUYER_PORTAL_URL}
+      />
+    );
+  }
 
   if (LOCAL_BUYER_PORTAL_HOST) {
     return (
